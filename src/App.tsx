14347efacc05169a4cafbd63895d5f900f20ad6d
Natthaya <66111594@dpu.ac.th>
2025-09-17 00:12:04 +0700

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import './App.css'
+import Counter from './Component/counter'
+import UseFrom from './Component/FromDate'
+
+function App() {
+
+  return (
+    <>
+      <Counter/>
+      <UseFrom />
+      
+   
+     
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/Component/counter.tsx
@@ -0,0 +1,33 @@
+import React, { useState }  from "react";
+
+interface CounterProps{
+    initialCount?:number;
+}
+
+const CounterButton:React.FC<CounterProps>=({initialCount=0})=>{
+    const[count,setCount] = useState<number>(initialCount);
+    
+     const increment =() =>{
+        setCount(prevCount=>prevCount+1);
+    }
+    const decrement =()=> {
+        setCount (count-1);
+    }
+    const handleClear = () => {
+        setCount (0);
+    }
+
+    return(
+        <div style={{padding:'20px',textAlign:'center'}}>
+            <h2>Counter Exercise-Solution</h2>
+            <h1 style={{color:'blueviolet'}}>{count}</h1>
+            <div style={{margin:'20px',}}>
+                 <button type="button" style={{backgroundColor:'red', margin: '10px'}} onClick={increment}>+เพิ่ม</button>
+                 <button type="button" style={{backgroundColor:'green', margin: '10px'}}onClick={decrement}>-ลด</button>
+                 <button type="button" style={{backgroundColor:'orange', margin: '10px'}}onClick={handleClear}>🔁Reset</button>
+            </div>
+        </div>
+    );
+};
+
+export default CounterButton;
